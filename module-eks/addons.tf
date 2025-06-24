efc0f6204cf2d89bb7f1448eddb020bdb9344ce0
sunrise-media <benjaminossai7@gmail.com>
2025-06-24 04:16:43 +0100

    addons issue

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,17 +1,17 @@
 provider "helm" {
-    kubernetes {
+    kubernetes = {
         host                   = aws_eks_cluster.eks.endpoint
         cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
         token                  = data.aws_eks_cluster_auth.eks.token
     }
 }
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   alias                  = "eks"
+#   host                   = aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
 
 data "aws_eks_cluster_auth" "eks" {
     name = aws_eks_cluster.eks.name
