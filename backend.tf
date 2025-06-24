e585ffbc4fc03a97d81051d2f5275946b7fc4d3f
sunrise-media <benjaminossai7@gmail.com>
2025-06-24 04:05:51 +0100

    new name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-solob-s3"
+    bucket         = "tf-solob-prod-terraform-001"
     key            = "envs/prod/terraform.tfstate"  # Customize this path as needed
     region         = "eu-north-1"
   }
